1c1c7336258e64781afcfdca8cf0be0989c5dc7e
cfy <1263473716@qq.com>
2021-01-18 00:29:45 +0800

    首页推荐景点完成

--- src/components/ScrollWrapper/Home.vue
+++ src/components/ScrollWrapper/Home.vue
@@ -2,24 +2,74 @@
   <div class="scroll-wrapper" ref="wrapper">
     <div class="scroll-content">
       <category-icons></category-icons>
+      <home-title :title="homeTitle.viewTitle"></home-title>
+      <view-list :viewDatas="homeDatas.viewDatas"></view-list>
+      <home-title :title="homeTitle.foodTitle"></home-title>
+      <home-title :title="homeTitle.hotelTitle"></home-title>
+      <home-title :title="homeTitle.massageTitle"></home-title>
+      <home-title :title="homeTitle.ktvTitle"></home-title>
     </div>
   </div>
 </template>
 
 <script>
-import CategoryIcons from './CategoryIcons';
-import BetterScroll from 'better-scroll';
+import CategoryIcons from "./CategoryIcons";
+import HomeTitle from "./Sub/HomeTitle";
+import ViewList from './ViewList';
+
+import BetterScroll from "better-scroll";
+import { IndexModel } from "models/index";
+import { mapState } from "vuex";
 export default {
-  name: 'HomeScrollWrapper',
+  name: "HomeScrollWrapper",
   components: {
-    CategoryIcons
+    CategoryIcons,
+    HomeTitle,
+    ViewList
   },
-  mounted(){
+  data(){
+    return {
+      homeTitle: {
+        foodTitle: '推荐美食',
+        hotelTitle: '推荐酒店',
+        ktvTitle: '推荐KTV',
+        massageTitle: '推荐按摩',
+        viewTitle: '推荐景点'
+      },
+      homeDatas:{
+        foodDatas: [],
+        hotelDatas: [],
+        ktvDatas: [],
+        massageDatas: [],
+        viewDatas: []
+      }
+    }
+  },
+  mounted() {
     this.scroll = new BetterScroll(this.$refs.wrapper);
-  }
-}
+    this.getHomeDatas(this.cityId);
+  },
+  computed: {
+    ...mapState(['cityId'])
+  },
+  methods: {
+    getHomeDatas(cityId) {
+      const indexModel = new IndexModel();
+      indexModel.getHomeDatas(cityId).then(res => {
+        if(res && res.status === 0){
+          const data = res.data;
+
+          this.homeDatas.foodDatas = data.foodDatas;
+          this.homeDatas.hotelDatas = data.hotelDatas;
+          this.homeDatas.ktvDatas = data.ktvDatas;
+          this.homeDatas.massageDatas = data.massageDatas;
+          this.homeDatas.viewDatas = data.viewDatas;
+        }
+      })
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
---
+++ src/components/ScrollWrapper/ViewList/Sub.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="view-item">
+    <router-link
+      :to="{ path: '/detail', query: { field: 'view', id: data.id } }"
+    >
+      <div class="img">
+        <img class="view-img" :src="data.img" :alt="data.name" />
+      </div>
+      <div class="info">
+        <h1 class="title">{{ data.name }}</h1>
+        <p class="stars">
+          <stars :starNum="Number(data.star)"></stars>
+          <span class="score">{{ data.score }}分</span>
+        </p>
+        <p class="others">
+          <span class="item"
+            >￥ <span class="price"> {{ data.default_price }} </span>起
+          </span>
+          <span class="item">{{ data.city_name }}</span>
+        </p>
+      </div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+import Stars from "../Sub/Stars";
+export default {
+  name: "ViewListSub",
+  components: {
+    Stars,
+  },
+  props: {
+    data: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~styles/mixins.scss";
+@import "~styles/variables.scss";
+
+.view-item {
+  width: 50%;
+  padding: 0.1rem;
+  box-sizing: border-box;
+
+  .img {
+    width: 100%;
+    height: 1.1rem;
+    overflow: hidden;
+
+    .view-img {
+      width: 100%;
+      min-height: 1.1rem;
+    }
+  }
+
+  .info {
+    .title {
+      @include ellipsis;
+      height: 0.3rem;
+      font-size: 0.16rem;
+      line-height: 0.3rem;
+      color: #333;
+    }
+
+    .stars {
+      height: 0.3rem;
+      line-height: 0.3rem;
+      font-size: 0.14rem;
+      color: #ccc;
+
+      .score {
+        color: $starColor;
+      }
+    }
+
+    .others {
+      display: flex;
+      justify-content: space-between;
+      height: 0.3rem;
+      line-height: 0.3rem;
+
+      .item {
+        font-size: 0.14rem;
+      }
+
+      .price {
+        font-size: 0.2rem;
+        color: $defaultGreen;
+        vertical-align: -0.01rem;
+      }
+    }
+  }
+}
+</style>
