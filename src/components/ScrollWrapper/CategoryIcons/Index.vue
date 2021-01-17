c0fed60ce32581f1802d84ce81cc20152341d20e
cfy <1263473716@qq.com>
2021-01-17 19:35:57 +0800

    首页分类导航栏完成

---
+++ src/components/ScrollWrapper/CategoryIcons/Index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="icons-wrapper">
+    <category-icons-sub
+      v-for="item of itemList"
+      :key="item.id"
+      :bgColor="item.bgColor"
+      :field="item.field"
+      :iconText="item.iconText"
+      :icon="item.icon"
+    >
+    </category-icons-sub>
+  </div>
+</template>
+
+<script>
+import CategoryIconsSub from './Sub';
+import fieldDatas from 'data/fields';
+
+import { mapState } from 'vuex'
+export default {
+  name: 'CategoryIcons',
+  components: {
+    CategoryIconsSub
+  },
+  data(){
+    return {
+      itemList: fieldDatas
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~styles/mixins.scss';
+@import '~styles/variables.scss';
+
+.icons-wrapper {
+  @include flex-row;
+  flex-wrap: wrap;
+  background-color: #fff;
+  border-bottom: 1px solid #ddd;
+}
+</style>
---
+++ src/components/ScrollWrapper/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="scroll-wrapper" ref="wrapper">
+    <div class="scroll-content">
+      <category-icons></category-icons>
+    </div>
+  </div>
+</template>
+
+<script>
+import CategoryIcons from './CategoryIcons';
+import BetterScroll from 'better-scroll';
+export default {
+  name: 'HomeScrollWrapper',
+  components: {
+    CategoryIcons
+  },
+  mounted(){
+    this.scroll = new BetterScroll(this.$refs.wrapper);
+  }
+}
+</script>
+
+<style>
+
+</style>
